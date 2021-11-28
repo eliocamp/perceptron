ecce162c88690ed395ad391bf92a58adac1f84fd
Elio Campitelli <eliocampitelli@gmail.com>
2021-11-28 16:56:18 -0300

    Agrega el ejemplo

--- perceptron.R
+++ perceptron.R
@@ -1,8 +1,7 @@
 perceptron_train <- function(X, y, epochs = 100, lambda = 10) {
-
-  X <- cbind(1, X)
+  X <- cbind(X)
   W <- rnorm(ncol(X))
-
+  Ws <- vector("list", epochs)
   accuracy <- rep(NA,  epochs)
 
   for (e in seq_len(epochs)) {
@@ -10,21 +9,21 @@ perceptron_train <- function(X, y, epochs = 100, lambda = 10) {
       y_hat <- sum(W * X[i, ])
 
       pred <- sign(as.numeric(y_hat >= 0) - 0.5)
-      # pred <- y_hat
 
       W <- W + lambda*(y[i] - pred)*X[i, ]
     }
-    W <- scale(W)
+    
+    W <- W/sqrt(sum(W^2))
     y_hat <- X %*% matrix(W, ncol = 1)
     pred <- sign(as.numeric(y_hat >= 0) - 0.5)
-    # pred <- y_hat
 
-    # accuracy[e] <- sd(y - pred)
     accuracy[e] <- mean((y - pred) == 0)
-
+    Ws[[e]] <- W
     cat(accuracy[e], "\n")
     if (accuracy[e] == 1) break
+    
   }
-  return(W)
+
+  return(Ws[[which.max(accuracy)]])
 }
 
